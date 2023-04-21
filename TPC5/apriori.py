c45f8fb57f3175f441376d4f58c09a2515f86114
joanaaVO <joanaveigaoliveira@gmail.com>
2023-04-21 22:59:11 +0100

    Mudanças TPC5

--- TPC5/apriori.py
+++ TPC5/apriori.py
@@ -1,7 +1,30 @@
 
-class Apriori:
-    def __init__(self, transactions, min_support, min_confidence):
+class TransactionDataset:
+    def __init__(self, transactions):
         self.transactions = transactions
+        self.frequent_items = {}
+        self.build_frequent_items()
+
+    def build_frequent_items(self):
+        item_counts = {}
+        for transaction in self.transactions:
+            for item in transaction:
+                if item in item_counts:
+                    item_counts[item] += 1
+                else:
+                    item_counts[item] = 1
+        for item, count in item_counts.items():
+            self.frequent_items[item] = count / len(self.transactions)
+    
+    def __iter__(self):
+        return iter(self.transactions)
+
+    def __len__(self):
+        return len(self.transactions)
+
+class Apriori:
+    def __init__(self, transaction_dataset, min_support, min_confidence):
+        self.transaction_dataset = transaction_dataset
         self.min_support = min_support
         self.min_confidence = min_confidence
         self.itemsets = {}
@@ -10,25 +33,25 @@ class Apriori:
     def fit(self):
         # Step 1: Get all unique items
         items = set()
-        for transaction in self.transactions:
+        for transaction in self.transaction_dataset:
             for item in transaction:
                 items.add(item)
-        
+
         # Step 2: Get frequent itemsets of length 1
         frequent_itemsets = {}
         for item in items:
-            count = sum([1 for transaction in self.transactions if item in transaction])
-            support = count / len(self.transactions)
+            count = sum([1 for transaction in self.transaction_dataset if item in transaction])
+            support = count / len(self.transaction_dataset)
             if support >= self.min_support:
                 frequent_itemsets[(item,)] = support
         self.itemsets = frequent_itemsets
-        
+
         # Step 3: Generate candidate itemsets
         k = 2
         candidate_itemsets = list(frequent_itemsets.keys())
         while candidate_itemsets:
             itemset_counts = {}
-            for transaction in self.transactions:
+            for transaction in self.transaction_dataset:
                 for itemset in candidate_itemsets:
                     if set(itemset).issubset(set(transaction)):
                         if itemset in itemset_counts:
@@ -37,7 +60,7 @@ class Apriori:
                             itemset_counts[itemset] = 1
             frequent_itemsets = {}
             for itemset, count in itemset_counts.items():
-                support = count / len(self.transactions)
+                support = count / len(self.transaction_dataset)
                 if support >= self.min_support:
                     frequent_itemsets[itemset] = support
             candidate_itemsets = self.generate_candidates(list(frequent_itemsets.keys()), k)
@@ -67,7 +90,7 @@ class Apriori:
                 itemset2 = set(itemsets[j])
                 if len(itemset1.union(itemset2)) == k:
                     candidate = tuple(sorted(itemset1.union(itemset2)))
-                    candidates.append(candidate)           
+                    candidates.append(candidate)
         return set(candidates)
 
 if __name__ == '__main__':
@@ -88,10 +111,11 @@ if __name__ == '__main__':
     min_support = 0.4
     min_confidence = 0.5
 
-    apriori = Apriori(transactions, min_support, min_confidence)
+    transaction_dataset = TransactionDataset(transactions)
+
+    apriori = Apriori(transaction_dataset, min_support, min_confidence)
     apriori.fit()
 
-    
     # Print frequent itemsets
     print("Frequent itemsets:")
     for itemset, support in apriori.itemsets.items():
