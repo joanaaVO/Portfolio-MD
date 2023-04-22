4b67896177a02cf40a405d8831ce1cd7a213850c
joanaaVO <joanaveigaoliveira@gmail.com>
2023-04-22 16:38:39 +0100

    Documentação TPC4

--- TPC4/naiveBayes.py
+++ TPC4/naiveBayes.py
@@ -8,12 +8,30 @@ from sklearn.model_selection import cross_val_score, train_test_split
 
 class NaiveBayes:
     def __init__(self, classes = None, mean = None, var = None, priors = None):
+        """
+        Initializes a new instance of the NaiveBayes class.
+
+        Parameters:
+            classes (array-like): The classes in the dataset.
+            mean (ndarray): The mean of each feature for each class.
+            var (ndarray): The variance of each feature for each class.
+            priors (ndarray): The a priori probabilities of each class.
+
+        """
         self.classes = classes
         self.mean = mean
         self.var = var
         self.priors = priors
 
     def fit(self, X, y):
+        """
+        Fits the NaiveBayes classifier to the given training data.
+
+        Args:
+            X (array-like, shape=(n_samples, n_features)): The training samples.
+            y (array-like, shape=(n_samples,)): The target values.
+
+        """
         n_samples, n_features = X.shape
         self.classes = np.unique(y)
         n_classes = len(self.classes)
@@ -31,6 +49,16 @@ class NaiveBayes:
             self.priors[i] = X_c.shape[0] / float(n_samples)
 
     def predict(self, X):
+        """
+        Predicts the target values for the given test data.
+
+        Args:
+            X (array-like, shape=(n_samples, n_features)): The test samples.
+
+        Returns:
+            predictions (ndarray, shape=(n_samples,)): The predicted target values.
+
+        """
         # Calculates the probability for each class for each instance
         posteriors = []
         for i, _ in enumerate(self.classes):
@@ -44,6 +72,18 @@ class NaiveBayes:
         return predictions
 
     def _pdf(self, X, mean, var):
+        """
+        Calculates the probability density function of a normal distribution.
+
+        Args:
+            X (array-like, shape=(n_samples, n_features)): The input data.
+            mean (ndarray, shape=(n_features,)): The mean of the distribution.
+            var (ndarray, shape=(n_features,)): The variance of the distribution.
+
+        Returns:
+            The probability density function of a normal distribution.
+
+        """
         # Probability density function of a normal distribution
         # It assumes that the features are independent
         numerator = np.exp(-((X-mean) ** 2) / (2 * (var + 1e-4)))
@@ -51,10 +91,30 @@ class NaiveBayes:
         return numerator / denominator
 
     def score(self, X, y):
+        """
+        Returns the mean accuracy on the given test data and labels.
+
+        Args:
+            X (array-like, shape=(n_samples, n_features)): The test samples.
+            y (array-like, shape=(n_samples,)): The true labels.
+
+        Returns:
+            The mean accuracy of the NaiveBayes classifier on the given test data and labels.
+
+        """
         y_pred = self.predict(X)
         return np.mean(y_pred == y)
 
     def get_params(self, deep=True):
+        """
+        Returns the parameters of the NaiveBayes classifier.
+
+        Args:
+            deep (boolean): Whether to return deep copies of the parameters.
+
+        Returns:
+            A dictionary containing the parameters of the NaiveBayes classifier.
+        """
         return {"classes": self.classes, "mean": self.mean, "var": self.var, "priors": self.priors}
 
 if __name__ == '__main__':
--- TPC4/prism.py
+++ TPC4/prism.py
@@ -6,7 +6,21 @@ from dataset import Dataset
 from sklearn.model_selection import train_test_split
 
 class Prism:
+    """
+    A class that implements the PRISM algorithm for inducing decision rules from data.
+
+    """
+
     def __init__(self, data=None, target=None, attributes=None):
+        """
+        Initialize a Prism instance.
+
+        Parameters:
+            data (array-like, shape (n_samples, n_features)): The data to induce rules from.
+            target (string): The name of the column containing the target variable.
+            attributes (list of strings): The names of the columns containing the attributes.
+
+        """
         self.data = data
         self.target = target
         self.attributes = attributes
@@ -14,6 +28,10 @@ class Prism:
         self.default_class = self.majority_class(self.data)
 
     def fit(self):
+        """
+        Induce the decision rules from the data.
+
+        """
         while True:
             best_rule = None
             best_coverage = 0
@@ -42,6 +60,16 @@ class Prism:
                 break
 
     def evaluate_rule(self, rule):
+        """
+        Evaluate the accuracy of a given rule on the data.
+
+        Parameters:
+            rule (dictionary): A rule, represented as a dictionary mapping attribute names to values.
+
+        Returns:
+            (float): The accuracy of the rule on the data.
+
+        """
         covered_data = [d for d in self.data if self.rule_covers(rule, d)]
         if not covered_data:
             return 0.0
@@ -50,19 +78,57 @@ class Prism:
         return correct_predictions / len(covered_data)
 
     def rule_covers(self, rule, data):
+        """
+        Check if a given rule covers a given data point.
+
+        Parameters:
+            rule (dictionary): A rule, represented as a dictionary mapping attribute names to values.
+            data (array-like, shape (n_features,)): The data point to check.
+
+        Returns:
+            (bool): Whether the rule covers the data point.
+
+        """
         return all(data[attr] == value for attr, value in rule.items())
 
     def majority_class(self, data):
+        """
+        Compute the most frequent class in the data.
+
+        Parameters:
+            data (array-like, shape (n_samples, n_features)): The data to compute the most frequent class from.
+
+        Returns:
+            (string): The most frequent class in the data.
+
+        """
         classes = [d[self.target] for d in data]
         return max(set(classes), key=classes.count)
 
     def predict(self, data):
+        """
+        Predict the class of a given data point using the induced rules.
+
+        Parameters:
+            data (array-like, shape (n_features,)): The data point to predict the class for.
+
+        Returns:
+            (string): The predicted class for the data point.
+
+        """
         for rule in self.rules:
             if self.rule_covers(rule, data):
                 return rule[self.target]
         return self.default_class
 
     def __repr__(self):
+        """
+        Return a string representation of the induced rules.
+
+        Returns:
+            rule_strings (string): The string representation of the induced rules.
+
+        """
         rule_strings = []
         for i, rule in enumerate(self.rules):
             rule_string = f"Rule {i+1}: "
